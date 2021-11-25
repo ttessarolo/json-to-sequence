855319386c1289014383b72849e4168cb696e743
Tommaso Tessarolo <tommaso@tessarolo.it>
2021-11-25 12:08:50 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,259 @@
+"use strict";
+import * as percom from "percom";
+import * as clone from "clone";
+import * as fs from "fs";
+
+const alphabet = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "Y",
+  "Z",
+];
+
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+  return array;
+}
+
+function getLength(alphabet, n) {
+  for (let k = 2; k < alphabet.length; k++) {
+    const p = percom.countPer(k, k);
+    if (p >= n) return k;
+  }
+}
+
+function getKeys(alphabet, n) {
+  const k = getLength(alphabet, n);
+  const alphabets = alphabet.slice(0, k);
+  const permutations = percom.per(alphabets, k);
+
+  return shuffleArray(permutations.map((p) => p.join("")));
+}
+
+function addToAlphabets(alphabet, seq) {
+  seq.forEach((s) => {
+    s.split("").forEach((k) => alphabet.add(k));
+  });
+}
+
+export default class JSONSequencer {
+  constructor({
+    model = {},
+    path,
+    fields = [],
+    skipFields = [],
+    filterValue = () => false,
+    fillNA = false,
+    uniformTokenLength = false,
+    NA = "X",
+    keyAlphabet = alphabet,
+    valuesAlphabet = numbers,
+    verbose = false,
+  }) {
+    this.model = model;
+    this.fields = fields;
+    this.skipFields = skipFields;
+    this.fillNA = fillNA;
+    this.uniformTokenLength = uniformTokenLength;
+    this.combinations = new Set();
+    this.NA = NA;
+    this.keyAlphabet = keyAlphabet;
+    this.valuesAlphabet = valuesAlphabet;
+    this.verbose = verbose;
+    this.filterValue = filterValue;
+
+    if (path) this.loadModel(path);
+  }
+
+  getCombinations() {
+    return this.combinations;
+  }
+
+  fit({ data }) {
+    const df = {
+      keys: new Map(),
+      keysSize: 0,
+      valuesSize: 0,
+      translators: {},
+      alphabets: new Set(),
+    };
+
+    data = Array.isArray(data) ? data : [data];
+    for (const row of data) {
+      for (let [key, value] of Object.entries(row)) {
+        if (!this.skipFields.includes(key)) {
+          if (!df.keys.has(key)) df.keys.set(key, new Set());
+          df.keysSize += 1;
+
+          const values = Array.isArray(value) ? value : [value];
+          for (const valore of values) {
+            df.keys.get(key).add(valore);
+          }
+        }
+      }
+    }
+
+    const keysAlpha = getKeys(this.keyAlphabet, df.keys.size);
+    addToAlphabets(df.alphabets, keysAlpha);
+
+    let keyLength = keysAlpha[0].length;
+    let valueLength = 0;
+    let k = 0;
+    let fixedValuesAlpha;
+
+    if (this.uniformTokenLength) {
+      let maxValueLength = 0;
+      for (const value of df.keys.values()) {
+        if (value.size > maxValueLength) maxValueLength = value.size;
+      }
+      fixedValuesAlpha = getKeys(this.valuesAlphabet, maxValueLength);
+    }
+
+    for (const [key, value] of df.keys.entries()) {
+      const valuesAlpha = fixedValuesAlpha ? shuffleArray(fixedValuesAlpha) : getKeys(value.size);
+
+      valueLength = valuesAlpha[0].length;
+      addToAlphabets(df.alphabets, valuesAlpha);
+
+      let v = 0;
+      const valuesMap = new Map();
+      for (const valore of [...value]) {
+        if (this.filterValue(key, valore)) continue;
+
+        df.valuesSize += 1;
+        valuesMap.set(valore, valuesAlpha[v]);
+        v = v + 1;
+      }
+
+      df.translators[key] = { key: keysAlpha[k], values: valuesMap };
+      k = k + 1;
+    }
+
+    df.alphabets = [...df.alphabets];
+
+    if (this.fillNA) {
+      df.alphabets.unshift(this.NA);
+    }
+
+    df.alphabets = df.alphabets.sort();
+
+    if (this.uniformTokenLength) {
+      df.keyLength = keyLength;
+      df.valueLength = valueLength;
+      df.tokenLength = keyLength + valueLength;
+    }
+
+    df.creationDate = Date.now();
+    delete df.keys;
+
+    this.model = df;
+
+    if (this.verbose) console.log(df);
+    return df;
+  }
+
+  transform(dataset) {
+    const results = [];
+    const multiple = Array.isArray(dataset);
+    if (!multiple) dataset = [dataset];
+
+    for (const data of dataset) {
+      const d = [];
+      for (let key of this.fields) {
+        if (!this.skipFields.includes(key)) {
+          const translate = this.model.translators[key];
+
+          if (!translate) {
+            throw new Error(`Key ${key} is not in the Model. Maybe you should refit it.`);
+          }
+
+          const value = data[key];
+
+          if (value) {
+            const values = Array.isArray(value) ? value : [value];
+            for (const valore of values) {
+              if (this.filterValue(key, valore)) continue;
+
+              const translated = translate.values.get(valore);
+              if (translated) {
+                const seq = `${translate.key}${translate.values.get(valore)}`;
+                d.push(seq);
+                this.combinations.add(seq);
+              } else
+                throw new Error(`Value ${valore} is not in the Model. Maybe you should refit it.`);
+            }
+          } else if (this.fillNA) {
+            if (this.uniformTokenLength)
+              d.push(translate.key + this.NA.repeat(this.model.valueLength));
+            else d.push(`${translate.key}${this.NA}`);
+          }
+        }
+      }
+
+      const seq = d.join("");
+      if (this.verbose) console.log(seq.length, seq);
+
+      results.push(seq);
+    }
+
+    return multiple ? results : results[0];
+  }
+
+  getAlphabets() {
+    if (this.model && this.model.alphabets) {
+      return this.model.alphabets;
+    }
+  }
+
+  saveModel(path) {
+    if (this.model) {
+      const model = clone.default(this.model);
+      Object.keys(model.translators).forEach((key) => {
+        model.translators[key].values = [...model.translators[key].values];
+      });
+
+      fs.writeFileSync(path, JSON.stringify(model, null, 1));
+    } else throw new Error("No Model To Save");
+  }
+
+  loadModel(path) {
+    const raw = fs.readFileSync(path);
+    const model = JSON.parse(raw);
+    Object.keys(model.translators).forEach((key) => {
+      model.translators[key].values = new Map(model.translators[key].values);
+    });
+
+    if (this.verbose) console.log(model);
+
+    this.model = model;
+  }
+}
